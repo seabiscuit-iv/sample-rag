0638f3a12925075b96d8960fcbb4df809d2a4630
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2024-03-01 19:58:13 +0000

    updated pinecone integration

--- pages/0_upload_pdf.py
+++ pages/0_upload_pdf.py
@@ -25,7 +25,7 @@ import os
 import streamlit as st
 from streamlit.logger import get_logger
 import PyPDF2
-import pinecone
+from pinecone import Pinecone
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 import hashlib
@@ -49,14 +49,15 @@ def pdf_to_text(uploaded_file):
     return text
 
 def embed(text,filename):
-    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+    # pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+    pinecone = Pinecone(api_key = PINECONE_API_KEY)
     index = pinecone.Index(PINECONE_INDEX_NAME)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap  = 200,length_function = len,is_separator_regex = False)
     docs=text_splitter.create_documents([text])
     for idx,d in enumerate(docs):
         hash=hashlib.md5(d.page_content.encode('utf-8')).hexdigest()
         embedding=client.embeddings.create(model="text-embedding-ada-002", input=d.page_content).data[0].embedding
-        metadata={"hash":hash,"text":d.page_content,"index":idx,"model":"text-embedding-ada-003","docname":filename}
+        metadata={"hash":hash,"text":d.page_content,"index":idx,"model":"text-embedding-ada-002","docname":filename}
         index.upsert([(hash,embedding,metadata)])
     return
 
